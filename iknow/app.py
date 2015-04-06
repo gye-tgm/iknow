8e4d0668d8793686fa2b1a9641010c3ceb0cbda2
Gary Ye <gye@student.tgm.ac.at>
2015-04-06 11:10:37 +0200

    Added database model

--- iknow/app.py
+++ iknow/app.py
@@ -3,45 +3,80 @@ RESTful webservice
 """
 from flask import Flask
 from flask.ext.restful import Api, Resource, reqparse
+from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
 api = Api(app)
 
-excerpts = {
-    {
-        'id': 0,
-        'book': 'A Song of Ice and Fire',
-        'excerpt': 'Winter is coming'
-    }
-}
+tags = db.Table('tags',
+                db.Column('tag_id', db.String, db.ForeignKey('tag.id')),
+                db.Column('knowledge_id', db.INTEGER,
+                          db.ForeignKey('knowledge.id')))
 
 
-class ExcerptListAPI(Resource):
-    def __init__(self):
-        self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('')
+class Knowledge(db.Model):
+    id = db.Column(db.INTEGER, primary_key=True, autoincrement=True)
+    content = db.Column(db.String(255))
+    tags = db.relationship('Tag', secondary=tags,
+                           backref=db.backref('knowledges', lazy='dynamic'))
 
-    def get(self):
-        pass
+    def __init__(self, content):
+        self.content = content
+
+    def __repr__(self):
+        return '<Content %r>' % self.content
+
+
+class Tag(db.Model):
+    id = db.Column(db.String, primary_key=True)
 
 
 class ExcerptAPI(Resource):
-    def get(self, id):
-        pass
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('content', type=str)
+        self.reqparse.add_argument('tags', type=str, actions='append')
+
+    def get(self):
+        args = self.reqparse.parse_args()
+        # TODO: multiple or single tags?
+        tag = Tag.query.filter_by(id=args['tag'])
+
+        l = []
+        for k in tag.knowledges:
+            l.append({'content': k.content})
+        return {'list': l}
 
     def put(self, id):
+        """
+        Update
+        """
         pass
 
     def delete(self, id):
+        """
+        Delete
+        """
         pass
 
-    def post(self, id):
-        pass
+    def post(self):
+        """
+        Stores a new knoledge entry into the database. The tags will also be
+        created.
+        """
+
+        args = self.reqparse.parse_args()
+        knowledge = Knowledge(args['content'])
+        knowledge.tags.extend(args['tags'])
+        db.session.add(knowledge)
+        db.session.commit()
 
 
-api.add_resource(ExcerptListAPI, '/excerpts')
-api.add_resource(ExcerptAPI, '/excerpts/<int:id>')
+api.add_resource(ExcerptAPI, '/excerpts')
 
 if __name__ == '__main__':
     app.run(debug=True)
