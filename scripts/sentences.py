11f5831cf2f1228b9268236b26d75e19144a5ac7
Gary Ye <gye@student.tgm.ac.at>
2015-04-16 13:08:16 +0200

    Added a 10^6 entry generator

---
+++ scripts/sentences.py
@@ -0,0 +1,32 @@
+from iknow.app import *
+from loremipsum import Generator
+import random
+import sys
+
+FILE = 'sentences.txt'
+# NUM_SENTENCES = 1000000
+NUM_SENTENCES = 10000
+
+dictfile = sys.argv[1]
+
+with open(dictfile, 'r') as dict_file:
+    dict = dict_file.read().split()
+
+g = Generator(dictionary=dict)
+
+for i in range(NUM_SENTENCES):
+    print(i)
+
+    sentence = g.generate_sentence()[2]
+    k = Knowledge(sentence) 
+    k.tags = []
+    tagstr = [random.choice(dict) for x in range(5)]
+    for t in tagstr:
+        x = Tag.query.filter_by(id=t).first()
+        if x is None:
+            x = Tag(t)
+            db.session.add(x)
+        k.tags.append(x)
+    db.session.add(k)
+    db.session.commit()
+
